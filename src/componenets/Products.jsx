68e3ac6559691444facabdf65ce281b7825a21fa
Yash Matade <105554335+YashMatade@users.noreply.github.com>
2022-11-23 15:09:05 +0530

    ..

--- src/componenets/Products.jsx
+++ src/componenets/Products.jsx
@@ -8,7 +8,6 @@ const Products = () => {
     const [Description, setDescription] = useState('');
     const [Post_Ids, setPost_Ids] = useState([]);
 
-
     useEffect(() => {
         axios({
             method: 'post',
@@ -32,35 +31,45 @@ const Products = () => {
     function handleChange(e) {
         const { checked, value } = e.target;
         if (checked) {
-            setPost_Ids([...Post_Ids, value])
+            setPost_Ids([...Post_Ids, value]);
         } else {
             setPost_Ids([Post_Ids.filter((e) => e !== value)]);
         }
     }
 
+
     function createPlaylist(e) {
-        
-        axios({
-            method: 'post',
-            url: "https://fxojmluid9.execute-api.ap-south-1.amazonaws.com/Prod/api/engt/getfeeds_v1",
-            headers: {
-                "X-api-key": "MXqO3cDcr492OTPGZZAot7akPvLmfKbA4bKt5Ryr",
-                "X-tenant-key": "DIVANOR123",
-            },
-            data: {
-                "PlayListId": 0,
-                "Post_Ids": Post_Ids,
-                "Name": Name,
-                "Description": Description
-            }
-        }).then((res) => {
-            console.log(res);
-            // alert(res);
-        }, (err) => {
-            console.log(err);
-        })
-        
+        e.preventDefault();
+        if (Name === "") {
+            alert('Enter Name');
+        }
+        else if (Description === "") {
+            alert('Enter Description');
+        } else {
+            alert('playlist created successfully');
+            axios({
+                method: 'post',
+                url: "https://fxojmluid9.execute-api.ap-south-1.amazonaws.com/Prod/api/engt/getfeeds_v1",
+                headers: {
+                    "X-api-key": "MXqO3cDcr492OTPGZZAot7akPvLmfKbA4bKt5Ryr",
+                    "X-tenant-key": "DIVANOR123",
+                },
+                data: {
+                    "PlayListId": 0,
+                    "Post_Ids": Post_Ids,
+                    "Name": Name,
+                    "Description": Description
+                }
+            }).then((res) => {
+                console.log(res);
+                alert(res);
+            }, (err) => {
+                console.log(err);
+            })
+        }
+
     }
+
 
     if (alldata !== undefined) {
         return (
@@ -74,13 +83,13 @@ const Products = () => {
                             onChange={(e) => setDescription(e.target.value)}
 
                         /></div>
-                        <div className="col-lg-4"><button className='btn btn-primary' onClick={createPlaylist()}>Create Playlist</button></div>
+                        <div className="col-lg-4"><button className='btn btn-primary' onClick={(e) => { createPlaylist(e) }}>Create Playlist</button></div>
                         {
                             alldata.map((data, key) => {
                                 return <div className="col-lg-3 mt-2">
-                                    <div className="card" style={{ width: "11rem" }}>
+                                    <div className="card" style={{ width: "11rem" ,height:"20rem" }}>
                                         <input class="form-check-input" type="checkbox" value={data.EngagementPostId} onChange={(e) => { handleChange(e) }} id="flexCheckChecked" />
-                                        <label class="form-check-label" for="flexCheckChecked">
+                                        <label class="form-check-label" htmlFor="flexCheckChecked">
                                             <img src={data.Thumbnail_URL} className="card-img-top" alt="..." />
                                             <div className="card-body">
                                                 <h5 className="card-title">{data.Thumbnail_Title}</h5>
